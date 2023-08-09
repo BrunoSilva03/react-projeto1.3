7ae84ded3124bcf9e130f96d08d2fe32a97255e9
Bruno Silva Rodrigues <78625466+BrunoSilva03@users.noreply.github.com>
2023-08-09 13:24:41 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Evento from './components/Evento'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Evento />
+      
+      
+    </>
+  )
+}
+
+export default App
