fb7ac00e7b138dd3cb033296ece7bec18620782f
Bruno Silva Rodrigues <78625466+BrunoSilva03@users.noreply.github.com>
2023-08-14 15:43:44 -0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Evento from './components/Evento'
+import OutraLista from './components/OutraLista'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const meusItens = ['React', 'Vue', 'Angular']
 
   return (
     <>
-     <Evento />
+    <h1>Renderização de Listas</h1>
       
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/>
       
     </>
   )
